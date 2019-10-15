dccb1d074db11c333d7c773eb8cce498079befb7
HDonovan96 <harry.bd11@gmail.com>
2019-10-15 17:38:27 +0100

    Created 'AbstractWoundedClass' and 'ReadWoundedData' to successully read 'WoundedNames.txt' into the project

--- Assets/Scripts/AbstractWOundedClass.cs
+++ Assets/Scripts/AbstractWOundedClass.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AbstractWOundedClass : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+using System.IO;
+using System;
 
-    // Update is called once per frame
-    void Update()
+public class AbstractWoundedClass : MonoBehaviour
+{
+    public string Name;
+    public string Age;
+    public string Rank;
+    public string Nationality;
+
+    public AbstractWoundedClass(string AllData = "", char Seperator = ',')
     {
+        if(AllData != "")
+        {
+            string[] items = AllData.Split(Seperator);
+
+            Name = items[0];
+            Age = items[1];
+            Rank = items[2];
+            Nationality = items[3];
+        }
         
     }
 }
---
+++ Assets/Scripts/ReadWoundedData.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+using System.IO;
+
+public class ReadWoundedData : AbstractWoundedClass
+{
+    public AbstractWoundedClass[] wounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextAsset woundedNamesAsset = Resources.Load<TextAsset>("Text Files/WoundedNames");
+        Debug.Assert(woundedNamesAsset != null, "WoundedNames.txt could not be loaded");
+
+        StringReader woundedNamesReader = new StringReader(woundedNamesAsset.text);
+
+        int lineCount = 0;
+        while (woundedNamesReader.ReadLine() != null)
+        {
+            lineCount++;
+        }
+        
+        wounded = new AbstractWoundedClass[lineCount];
+        woundedNamesReader = new StringReader(woundedNamesAsset.text);
+
+        for(int i = 0; i < lineCount; i++)
+        {
+            wounded[i] = new AbstractWoundedClass(woundedNamesReader.ReadLine());
+        }
+
+        Debug.Log(wounded[0].Name);
+        Debug.Log(wounded[1].Name);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
