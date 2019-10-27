8ef1049e5ac9286c83577acc9a2947c8d2217d14
HDonovan96 <harry.bd11@gmail.com>
2019-10-27 12:29:11 +0000

    Added more comments to code

--- Text File Generator/GenTextFile.py
+++ Text File Generator/GenTextFile.py
@@ -1,3 +1,5 @@
+#Creates a text file filled with placeholder names and nationalities
+#Can be updated to use actual names and dates
 temp = open('..\\Assets\\Resources\\Text Files\\WoundedNames.txt', 'w')
 for i in range(1, 100):
     temp.write("Name" + str(i) + ", Nationality" + str(i) + "\n")
